112bdf5a99ab8af95a6eb5b40824b2b59516f3be
Tester <tester@example.com>
2026-02-04 13:24:29 +0100

    pass

---
+++ task-1/post.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -X POST http://localhost:3000/users \
+  -H "Content-Type: application/json; charset=UTF-8" \
+  -d '{
+    "name": "John Doe",
+    "email": "john.doe@example.com",
+    "password": "secret",
+    "role": "user",
+    "active": true,
+    "department": "Engineering"
+  }'
